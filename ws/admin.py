6e932a060c607148edd5cf2c96a3b637fe34f8db
brian huynh <huynh2102@gmail.com>
2024-03-17 10:50:31 +0700

    register models

--- ws/admin.py
+++ ws/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Category, Customer, MenuItem, Order, OrderItem, Staff
+
 # Register your models here.
+
+admin.site.register(Staff)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(OrderItem)
