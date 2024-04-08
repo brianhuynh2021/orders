02d5130ea9767890bb8ff0908de97b978aaa73bf
Truong Trong Hai <truong.hai@del-qui.com>
2024-04-08 10:05:12 +0700

    make change to run Daphne server

--- order/asgi.py
+++ order/asgi.py
@@ -14,12 +14,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from ws.routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "order.settings")
 
 django_asgi_app = get_asgi_application()
-# application = get_asgi_application()
+# line below MUST import after setting DJANGO_SETTINGS_MODULE and get_asgi_application() because the way Django loading settings before loading apps
+from ws.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
